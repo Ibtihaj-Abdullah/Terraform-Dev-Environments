1bec18ce9448b8efd99e22d3081112fc29d01764
Ebtihaj <ebti5098561@gmail.com>
2025-02-06 16:33:04 +0500

    A3

--- A1/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-a1-bucket" 
-    key            = "tfstates/terraform.tfstate"          
-    region         = "us-west-2"                          
-    encrypt        = true                                 
-    dynamodb_table = "terraform-a1-state-lock"               
-  }
-}
